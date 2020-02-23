31329cc6ffecd3d9aec422b47e1c0d7b1c10e4e9
Jason Tuttle <ron.tuttle@g.austincc.edu>
2020-02-23 17:53:12 -0600

    Ran a program practicing std::cout with block letters.

---
+++ CA-Block-Letters/CA-Block-Letters.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "JJJJJ TTTTT\n";
+	std::cout << "  J     T  \n";
+	std::cout << "  J     T  \n";
+	std::cout << "  J     T  \n";
+	std::cout << "J J     T  \n";
+	std::cout << "J J     T  \n";
+	std::cout << " JJ     T  \n";
+
+	return 0;
+}
