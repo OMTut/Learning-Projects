99ea528eb97e6a99f876189033e68178ca0adaab
Jason Tuttle <ron.tuttle@g.austincc.edu>
2020-02-27 11:45:25 -0600

    Converted the project to a CMake project so I can practice with cmake while I work in linux.

---
+++ cmake-build-debug/CMakeFiles/Learning_Projects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Learning_Projects.dir/CA-03-DollarConversion/CA-03-DollarConversion.cpp.o"
+  "Learning_Projects"
+  "Learning_Projects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Learning_Projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
